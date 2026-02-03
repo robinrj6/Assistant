7e9bef25268fa2beadada2c94aa7c206cdd68618
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-02-03 14:20:11 +0100

    update: stop with ctrl+c

--- main.sh
+++ main.sh
@@ -1,4 +1,10 @@
 source fastapi-env/bin/activate
+
+# Trap Ctrl+C to kill both processes
+trap 'kill $(jobs -p) 2>/dev/null' EXIT INT TERM
+
 uvicorn backend.main:app --reload &
 cd frontend
 npm run dev
+
+wait
